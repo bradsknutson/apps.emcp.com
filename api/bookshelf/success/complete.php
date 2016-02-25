b6852d1254551b315943662c7b7cd6ae2ef8cecf
Brad Knutson <brad.s.knutson@gmail.com>
2016-02-25 10:31:42 -0600

    Initial Release of IDS/SFDC App

--- api/bookshelf/success/complete.php
+++ api/bookshelf/success/complete.php
@@ -1,14 +1,3 @@
-<?php
-
-    include '../inc/functions.php';
-
-    $account_id = getID($email, $mysqli);
-    $campaign_vars = getCampaignVars($email, $campaign_id, $mysqli);
-
-    $book_id = $campaign_vars[0]['book_id'];
-
-?>
-<!DOCTYPE html>
 <html lang="en-US">
     <head>
         <meta charset="utf-8">
@@ -23,23 +12,10 @@
             <div class="row">
                 <div class="col-sm-12 col-md-3"></div>
                 <div class="col-sm-12 col-md-6">
-                    <?php if( $email && $campaign_id ) { ?>
                     <div class="jumbotron">
                         <h1>Thanks for your interest!</h1>
                         <p>Your eBook is on it's way.  One moment while we redirect you.</p>
                     </div>
-                    <form class="linkbook" action="<?php echo $goToBookURL; ?>" method="GET">
-                        <input id="book_id" name="book_id" value="<?php echo $book_id; ?>" />
-                        <input id="account_id" name="account_id" value="<?php echo $account_id; ?>" />   
-                        <input id="application_name" name="application_name" value="SFDC" />                     
-                    </form>
-                    <script type="text/javascript">
-                        $(document).ready(function(){  
-                            setTimeout(function(){
-                                $('.linkbook').submit();
-                            },1000);
-                        });
-                    </script>
                     <div class="row loading-row">
                         <div class="loading">
                             <div class="loader">
@@ -50,12 +26,6 @@
                             </div>
                          </div>
                     </div>
-                    <?php } else { ?>
-                    <div class="jumbotron">
-                        <h1>Something went wrong.</h1>
-                        <p>Something's not working properly. Please try again.</p>
-                    </div>
-                    <?php } ?>
                 </div>
                 <div class="col-sm-12 col-md-3"></div>
             </div>
